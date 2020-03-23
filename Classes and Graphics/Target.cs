41f2f69961c5013fcf321874edf1afcc0cd8c4ff
AldworthClass <46466802+AldworthClass@users.noreply.github.com>
2020-03-23 11:35:35 -0400

    overloaded Distance()

--- Classes and Graphics/Target.cs
+++ Classes and Graphics/Target.cs
@@ -17,6 +17,10 @@ namespace Classes_and_Graphics
         private int hSpeed;
         private int vSpeed;
 
+        public static double Distance(int x1, int y1, int x2, int y2)
+        {
+            return Distance(new Point(x1, y1), new Point(x2, y2));
+        }
         public static double Distance(Point p1, Point p2)
         {
             return Math.Sqrt((p1.X - p2.X)* (p1.X - p2.X) + (p1.Y - p2.Y)* (p1.Y - p2.Y));
